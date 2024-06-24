4b3b59f08171c268e147153ec060f37549204b37
Frederico Muinhos <fred.cal@hotmail.com>
2024-06-24 15:30:40 -0400

    feat(client): FE initial commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => {
+        console.log(res);
+        this.users = res;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+      complete: () => {
+        console.log('Request has completed!');
+      },
+    });
+  }
+}

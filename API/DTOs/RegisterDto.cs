77bfdd581b18a148604f1886235d003ce3963b17
Frederico Muinhos <fred.cal@hotmail.com>
2024-06-26 18:29:50 -0400

    feat(errorHandling): Implemented error handler

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,10 +4,11 @@ namespace API;
 
 public class RegisterDto
 {
-  [Required]
-  [MaxLength(100)]
-  public required string Username { get; set; }
-  
-  [Required]
-  public required string Password { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Username { get; set; } = string.Empty;
+      
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; } = string.Empty;
 }
